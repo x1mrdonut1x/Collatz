2a03f2364869a4241227004020db26d716ccac24
Alex Niznik <alex@niznik.info>
2016-09-03 13:01:22 -0500

    created UVaCollatz.cpp

---
+++ UVaCollatz.cpp
@@ -0,0 +1,78 @@
+#include <cassert>  // assert
+#include <iostream> // endl, istream, ostream
+
+using namespace std;
+
+
+
+// ------------
+// collatz_read
+// ------------
+
+bool collatz_read (istream& r, int& i, int& j) {
+    if (!(r >> i))
+        return false;
+    r >> j;
+    return true;}
+
+//-------------
+//cycle_length
+//-------------
+int cycle_length (int n) {
+
+    int c = 1;
+    while (n > 1) {
+        if ((n % 2) == 0)
+            n = (n / 2);
+        else
+            n = (3 * n) + 1;
+        ++c;}
+
+    return c;}
+
+// ------------
+// collatz_eval
+// ------------
+
+int collatz_eval (int i, int j) {
+
+    if (j < i){
+        int temp = i;
+        i = j;
+        j = temp;
+    }
+
+    int max_cycle = 0;
+
+    for (i; i<=j && i > 0; ++i){
+        if(max_cycle < cycle_length(i))
+            max_cycle = cycle_length(i);
+    }
+
+    return max_cycle;}
+
+// -------------
+// collatz_print
+// -------------
+
+void collatz_print (ostream& w, int i, int j, int v) {
+    w << i << " " << j << " " << v << endl;}
+
+// -------------
+// collatz_solve
+// -------------
+
+void collatz_solve (istream& r, ostream& w) {
+    int i;
+    int j;
+    while (collatz_read(r, i, j)) {
+        const int v = collatz_eval(i, j);
+        collatz_print(w, i, j, v);}}
+// ----
+// main
+// ----
+
+int main () {
+    using namespace std;
+    collatz_solve(cin, cout);
+    return 0;}
